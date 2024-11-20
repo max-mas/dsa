3303e7871b569ed040f35b03f95b405fbe459f9c
Elise Pilgermann <e.pilgermann@tu-bs.de>
2024-11-20 22:11:17 +0100

    save plot to file and set dpi

--- u02/wuerfeln.py
+++ u02/wuerfeln.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 data = np.loadtxt('wuerfel.txt')
 n_bins = round(10 / 0.01) # because the biggest and smallest possible are 12 and 2
 
-fig, axs = plt.subplots(5, 1, tight_layout=False)
+fig, axs = plt.subplots(5, 1, tight_layout=False, dpi=300)
 
 for i in range(5):
     axs[i].hist(data[:, i], bins=n_bins, range=(2,12))
 
 fig.show()
+fig.tight_layout()
+fig.savefig("die.png")
 
 for i in range(5):
     print(np.std(data[:, i]))
