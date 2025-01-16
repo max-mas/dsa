a300a95cef88673b44112e0fe34ea649c77d9b22
Max Maschke <80268744+max-mas@users.noreply.github.com>
2025-01-16 21:30:26 +0100

    work

--- u05/u05.py
+++ u05/u05.py
@@ -1,10 +1,13 @@
 # external imports
 import numpy as np
 import scipy.signal as sig
+from scipy.ndimage import maximum_filter
+from scipy.ndimage import generate_binary_structure, binary_erosion
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 # STL imports
+import sys
 
 # mpl defaults
 plt.rcParams.update({
@@ -21,22 +24,66 @@ plt.rcParams.update({
 })
 sns.color_palette("colorblind")
 
+def set_bit(value, n):
+    return value | (1 << n)
+
 def main():
     message = np.loadtxt("message.txt")
     N = len(message)
 
-    win = sig.windows.hamming(160, sym=True)
-    SFT = sig.ShortTimeFFT(win=win, hop=50, fs=1/8000, fft_mode="onesided2X", scale_to="psd")
-    stft = SFT.stft(message)
+    data_fs = [697, 770, 852, 941, 1209, 1336, 1477, 1633]
 
-    fig, ax = plt.subplots()
-    ax.imshow(np.abs(stft))
-    t_lo, t_hi = SFT.extent(N)[:2]
-    ax.set( xlabel=f"Time $t$ in seconds ({SFT.p_num(N)} slices, " + rf"$\Delta t = {SFT.delta_t:g}\,$s)", 
-            ylabel=f"Freq. $f$ in Hz ({SFT.f_pts} bins, " + rf"$\Delta f = {SFT.delta_f:g}\,$Hz)", 
-            xlim=(t_lo, t_hi))
+    win = sig.windows.hamming(400, sym=True)
+    SFT = sig.ShortTimeFFT(win=win, hop=400, fs=8000, fft_mode="onesided2X", scale_to="psd") # 400 = 50 ms
+    stft = SFT.stft(message)
+    psd = np.abs(stft)
+
+    fig, ax = plt.subplots(figsize=(10,7))
+    im = ax.imshow(psd, aspect="auto", origin="lower", extent=SFT.extent(N), cmap="Greens")
+    ax.hlines(data_fs, xmin=0, xmax=N/8000,  colors="indianred", linestyles="dashed", label="Bit Frequencies")
+    ax.set_ylabel("Frequency $f$ (Hz)")
+    ax.set_xlabel("Time $t$ (s)")
+    fig.colorbar(im, label="Spectral Density (a.u.)")
+    ax.set_ylim(500, 2000)
+    ax.legend()
 
     fig.savefig("test.pdf")
+    plt.close(fig)
+
+    fig, ax = plt.subplots(figsize=(10,7))
+    mask = psd>0.035
+    im = ax.imshow(mask, aspect="auto", origin="lower", extent=SFT.extent(N), cmap="Greens")
+    ax.hlines(data_fs, xmin=0, xmax=N/8000,  colors="indianred", linestyles="dashed", label="Bit Frequencies")
+    ax.set_ylabel("Frequency $f$ (Hz)")
+    ax.set_xlabel("Time $t$ (s)")
+    fig.colorbar(im, label="Spectral Density (a.u.)")
+    ax.set_ylim(500, 2000)
+    ax.legend()
+
+
+    fig.savefig("test2.pdf")
+
+    ticks = psd.shape[1]
+    indices = [int(data_f/4020 * psd.shape[0]) for data_f in data_fs]
+    print(indices)
+
+    message = ""
+    for i in range(0, ticks-1, 2):
+        char = 0
+        first_half = mask[indices, i]
+        second_half = mask[indices, i+1]
+        for loc in range(0, 4):
+            val = first_half[-(4-loc)]
+            char = set_bit(char, loc) if val else char
+            val = second_half[-(4-loc)]
+            char = set_bit(char, loc+4) if val else char
+        print(bin(char))
+        message += chr(char)
+
+    print(message)
+        
+
+    
 
 if __name__ == "__main__":
     main()
